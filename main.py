17a73446a4e4ffdf340e68d64efcbc1c5d2c60c5
Ramon Arias <115386043+itsrarias@users.noreply.github.com>
2023-05-18 17:19:55 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+    num_indices = {}
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in num_indices:
+            return [num_indices[complement], i]
+        num_indices[num] = i
+    return []  # Return an empty list if no solution is found
